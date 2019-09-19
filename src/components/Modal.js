be1812755ed161bde390bd9f2b323c5165681567
Carlo Zablan <zablanc91@hotmail.com>
2019-09-18 21:56:48 -0700

    added modal to display in StreamDelete

---
+++ src/components/Modal.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import history from '../history';
+
+const Modal = props => {
+    //arguments to createPortal: jsx to display, and container element
+    return ReactDOM.createPortal(
+        <div className="ui dimmer modals visible active" onClick={() => history.push('/')} >
+            <div className="ui standard modal visible active" onClick={(e) => e.stopPropagation() }>
+                <div className="header">
+                    Delete Stream
+                </div>
+                <div className="content">
+                    Are you sure you want to delete this stream?
+                </div>
+                <div className="actions">
+                    <button className="ui primary button">Delete</button>
+                    <button className="ui button">Cancel</button>
+                </div>
+            </div>
+        </div>,
+        document.querySelector('#modal')
+    );
+};
+
+export default Modal;
