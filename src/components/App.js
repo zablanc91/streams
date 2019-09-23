4b34cf5eaafbce4914a0ec9ef4f341c047039034
Carlo Zablan <zablanc91@hotmail.com>
2019-09-22 17:58:37 -0700

    implemented data from StreamShow, app code for streams functionality is complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
@@ -10,19 +10,20 @@ import history from '../history';
 
 //client ID: 530819572065-cib033liuvgqrelq1396u8ok3dte2nql.apps.googleusercontent.com
 
-
-
+//Switch wraps around Routes so that StreamShow will not be displayed when StreamCreate is open (issue because of :id wildcard)
 const App = () => {
     return(
         <div className="ui container">
             <Router history={history} >
                 <div>
                     <Header />
-                    <Route path="/" exact component={StreamList} />
-                    <Route path="/streams/new" exact component={StreamCreate} />
-                    <Route path="/streams/edit/:id" exact component={StreamEdit} />
-                    <Route path="/streams/delete/:id" exact component={StreamDelete} />
-                    <Route path="/streams/show" exact component={StreamShow} />
+                    <Switch>
+                        <Route path="/" exact component={StreamList} />
+                        <Route path="/streams/new" exact component={StreamCreate} />
+                        <Route path="/streams/edit/:id" exact component={StreamEdit} />
+                        <Route path="/streams/delete/:id" exact component={StreamDelete} />
+                        <Route path="/streams/:id" exact component={StreamShow} />
+                    </Switch>
                 </div>
             </Router>
         </div>
